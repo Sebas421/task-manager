94a451a1e8e680c8a06ce136a37ebff5bfec72fe
Sebas421 <Slh.492@gmail.com>
2025-06-13 12:42:10 -0500

    Fix

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -15,18 +15,38 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert('Formulario enviado:\n' + JSON.stringify(formData, null, 2));
+    alert('Form submitted:\n' + JSON.stringify(formData, null, 2));
     setFormData({ firstName: '', lastName: '', email: '', comments: '' });
   };
 
   return (
     <form onSubmit={handleSubmit} className="contact-form">
-     <input name="firstName" placeholder="First Name" />
-     <input name="lastName" placeholder="Last Name" />
-     <input name="email" placeholder="Email" />
-     <textarea name="comments" placeholder="Comments" />
-     <button type="submit">Submit</button>
-
+      <h2>Contact Us</h2>
+      <input
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+        placeholder="First Name"
+      />
+      <input
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+        placeholder="Last Name"
+      />
+      <input
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      <textarea
+        name="comments"
+        value={formData.comments}
+        onChange={handleChange}
+        placeholder="Comments"
+      />
+      <button type="submit">Submit</button>
     </form>
   );
 };
