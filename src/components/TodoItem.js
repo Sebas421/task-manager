aabfe83cb1e0977f3fd76ee0ac84789365c52efd
Sebas421 <Slh.492@gmail.com>
2025-06-17 09:29:09 -0500

    Add 'Mark as complete' functionality to todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,14 +3,16 @@ import React from 'react';
 const TodoItem = ({ todo, toggleComplete, removeTodo }) => (
   <li className="todo-item">
     <span
-      onClick={() => toggleComplete(todo.id)}
       style={{
         textDecoration: todo.completed ? 'line-through' : 'none',
-        cursor: 'pointer'
+        flex: 1
       }}
     >
       {todo.text}
     </span>
+    <button onClick={() => toggleComplete(todo.id)}>
+      {todo.completed ? 'Undo' : 'Mark as complete'}
+    </button>
     <button onClick={() => removeTodo(todo.id)}>❌</button>
   </li>
 );
